e28717dbe8c3450d45e994550c09e121b63ad002
bazhil <i-bazh@mail.ru>
2020-05-26 02:43:59 +0300

    Add pages for works with author forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import *
 from django.urls import path
 from catalog.views import index, BookListView, BookDetailView, AuthorListView, AuthorDetailView, \
-    LoanedBooksByUserListView, LoanedBooks
+    LoanedBooksByUserListView, LoanedBooks, renew_book_librarian, AuthorCreate, AuthorUpdate, AuthorDelete
 
 
 
@@ -15,4 +15,8 @@ urlpatterns = [
     url(r'^author/(?P<pk>\d+)$', AuthorDetailView.as_view(), name='author-detail'),
     url(r'^mybooks/$', LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     url(r'^borrowed-books/$', LoanedBooks.as_view(), name='borrowed-books'),
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', renew_book_librarian, name='renew-book-librarian'),
+    url(r'^author/create/$', AuthorCreate.as_view(), name='author_create'),
+    url(r'^author/(?P<pk>\d+)/update/$', AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/delete/$', AuthorDelete.as_view(), name='author_delete'),
 ]
